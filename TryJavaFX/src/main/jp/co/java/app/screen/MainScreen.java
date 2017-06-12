87620c99b46a8831379ae937ed95e00fb22cde72
tk <tk@example.com>
2017-06-13 00:11:17 +0900

    Create Initial Project

---
+++ TryJavaFX/src/main/jp/co/java/app/screen/MainScreen.java
@@ -0,0 +1,25 @@
+
+package main.jp.co.java.app.screen;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+//import javafx.scene.Scene;
+
+
+public class MainScreen extends Application{
+	
+	public static void main(String[] args){
+		
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception{
+		
+		
+	}
+	
+}
+
+
+
+
