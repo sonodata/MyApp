c4e07f0b90ea6cf25d87829bfd1e17c1fca322e6
tk <tk@example.com>
2017-06-15 02:00:34 +0900

    modified for showing White Screen.

--- TryJavaFX/src/main/jp/co/java/app/screen/MainScreen.java
+++ TryJavaFX/src/main/jp/co/java/app/screen/MainScreen.java
@@ -3,21 +3,34 @@ package main.jp.co.java.app.screen;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-//import javafx.scene.Scene;
-
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;;
 
 public class MainScreen extends Application{
 	
 	public static void main(String[] args){
 		
+		launch(args);
+		
 	}
+	
 	
 	@Override
-	public void start(Stage stage) throws Exception{
+	public void start(Stage primStage) throws Exception{
 		
-		
+	  try{
+		  
+		  BorderPane root = new BorderPane();
+		  Scene scene = new Scene(root,400,400);
+		  primStage.setScene(scene);
+		  primStage.show();
+		  
+	  }	catch(Exception e ){
+		  
+		  e.printStackTrace();
+		  
+	  }
 	}
-	
 }
 
 
