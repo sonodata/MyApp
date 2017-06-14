4d8a543ec11ede4d80119e284e01a082b6266e3c
tk <tk@example.com>
2017-06-15 01:45:40 +0900

    add util folder and fileutil

---
+++ TryJavaFX/src/main/jp/co/java/util/file/ReadFile.java
@@ -0,0 +1,28 @@
+
+package main.jp.co.java.util.file;
+
+import java.util.ArrayList;
+
+
+public class ReadFile{
+	
+	
+	/**
+	 * 
+	 * @param path　読み込み対象のファイルパス
+	 * @return 読み込んだファイルのテキストデータ
+	 */
+	public ArrayList<String> ReadAnyFile(String path){
+		
+		ArrayList<String> readStr = new ArrayList<String>();
+		
+		
+		
+		return readStr;
+	}
+	
+	
+	
+}
+
+
