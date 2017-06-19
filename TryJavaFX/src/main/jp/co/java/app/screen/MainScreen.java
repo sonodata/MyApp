c802f282cadc20b7f135335a57b6bc699aa26090
tk <tk@example.com>
2017-06-20 03:07:02 +0900

    modify for try PullDown ConmboBox from properties.

--- TryJavaFX/src/main/jp/co/java/app/screen/MainScreen.java
+++ TryJavaFX/src/main/jp/co/java/app/screen/MainScreen.java
@@ -1,13 +1,18 @@
 
 package main.jp.co.java.app.screen;
 
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
@@ -29,6 +34,13 @@ public class MainScreen extends Application{
 		  TextField textField = new TextField("0");
 		  Button clickButton = new Button("Click");
 
+		  
+		  //入力コンボボックスの作成
+		  //入力候補要素の作成
+		  
+		  ComboBox inputComboBox = new ComboBox(readyInputElem());
+		  
+
           clickButton.setOnAction(
         		  e -> {topLabel.setText("Inputted \"" + textField.getText() + "\"");}
         		  );
@@ -43,11 +55,13 @@ public class MainScreen extends Application{
 				  topLabel.setText(msg);
 			  }
 		  });
-*/		  		  
+*/		  		
+          
 		  BorderPane rootPane = new BorderPane();
 		  
 		  rootPane.setTop(topLabel);
-		  rootPane.setCenter(textField);
+		  //rootPane.setCenter(textField);
+		  rootPane.setCenter(inputComboBox);
 		  rootPane.setBottom(clickButton);
 		  Scene scene = new Scene(rootPane,200,200);
 		  primStage.setScene(scene);
@@ -59,6 +73,24 @@ public class MainScreen extends Application{
 		  
 	  }
 	}
+	
+	private ObservableList<String> readyInputElem(){
+		
+		ObservableList<String> elemList = FXCollections.observableArrayList();
+		
+		ResourceBundle resource = ResourceBundle.getBundle("inputElem");
+		
+		int elemNum = Integer.parseInt(resource.getString("ElemNum"));
+		for(int i = 1;i <= elemNum;i++){
+			String elem = resource.getString("InputElem" + i);
+			elemList.add(elem);	
+		}
+		
+		return elemList;
+	}
+	
+	
+	
 }
 
 
